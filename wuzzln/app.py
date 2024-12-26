45794fff0f8ecfd374595b5594e7c4c0abf86d4a
schiegl <schiegl.adrian@gmail.com>
2024-12-26 11:49:48 +0100

    add season review

--- wuzzln/app.py
+++ wuzzln/app.py
@@ -17,12 +17,19 @@ from wuzzln.routes.leaderboard import get_leaderboard_page
 from wuzzln.routes.matchmaking import get_matchmaking_page, post_matchmaking
 from wuzzln.routes.player import get_player_page
 from wuzzln.routes.rules import get_rules_page
+from wuzzln.routes.wrapped import get_wrapped_page, get_wrapped_season
 
 logging_config = LoggingConfig(
     root={"level": "DEBUG"},
     formatters={"standard": {"format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"}},
     log_exceptions="always",
 )
+
+
+def register_template_callables(engine: JinjaTemplateEngine):
+    engine.register_template_callable(
+        key="get_wrapped_season", template_callable=get_wrapped_season
+    )
 
 
 app = Litestar(
@@ -38,6 +45,7 @@ app = Litestar(
         get_games_page,
         get_rules_page,
         get_player_page,
+        get_wrapped_page,
     ],
     static_files_config=[
         StaticFilesConfig(directories=["assets/img"], path="img"),
@@ -50,6 +58,7 @@ app = Litestar(
     template_config=TemplateConfig(
         directory=Path("templates"),
         engine=JinjaTemplateEngine,
+        engine_callback=register_template_callables,
     ),
     logging_config=logging_config,
 )
---
+++ wuzzln/routes/wrapped.py
@@ -0,0 +1,143 @@
+import sqlite3
+from collections import defaultdict
+from collections.abc import Iterable, Mapping
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+from typing import Any
+
+from litestar import get
+from litestar.connection import ASGIConnection
+from litestar.exceptions import NotAuthorizedException
+from litestar.handlers import BaseRouteHandler
+from litestar.response import Template
+
+from wuzzln.data import Game, PlayerId, SeasonId, get_season
+
+
+@dataclass
+class Award:
+    emoji: str
+    title: str
+    description: str
+    player: PlayerId
+
+
+def get_game_score_awards(games: Iterable[Game], prior_games: dict[PlayerId, int]) -> list[Award]:
+    """Get all awards that relate to the game scores
+
+    :param games: list of games
+    :param prior_games: games each player played prior to the current games
+    :return: list of awards
+    """
+    total_games = defaultdict(int)
+    total_1v1s = defaultdict(int)
+    total_crawls = defaultdict(int)
+    total_let_crawls = defaultdict(int)
+    total_played_others = defaultdict(set)
+
+    for g in games:
+        # total games
+        players = {g.defense_a, g.offense_a, g.defense_b, g.offense_b}
+        for p in players:
+            total_games[p] += 1
+            total_played_others[p] = total_played_others[p] | players
+
+        # 1v1s
+        if len(players) == 2:
+            for p in players:
+                total_1v1s[p] += 1
+
+        # crawling
+        team_a = {g.defense_a, g.offense_a}
+        team_b = {g.defense_b, g.offense_b}
+        result = [(team_a, team_b, g.score_a, g.score_b), (team_b, team_a, g.score_b, g.score_a)]
+        for players, other_players, score, other_score in result:
+            if (
+                score == 0
+                and other_score > 0
+                and all(total_games.get(p, 0) + prior_games.get(p, 0) >= 25 for p in players)
+            ):
+                for p in players:
+                    total_crawls[p] += 1
+            if (
+                other_score == 0
+                and score > 0
+                and all(total_games.get(p, 0) + prior_games.get(p, 0) >= 25 for p in other_players)
+            ):
+                for p in players:
+                    total_let_crawls[p] += 1
+
+    awards = []
+    if total_games:
+        player = max(total_games, key=total_games.get)  # type: ignore
+        awards.append(Award("🚵", "Going Pro", f"Played {total_games[player]:,d} games", player))
+
+    if total_played_others:
+        player = max(total_played_others, key=lambda p: len(total_played_others[p]))  # type: ignore
+        awards.append(
+            Award(
+                "🌍",
+                "Globalist",
+                f"Played with {len(total_played_others[player]):,d} different people",
+                player,
+            )
+        )
+
+    if total_1v1s:
+        player = max(total_1v1s, key=total_1v1s.get)  # type: ignore
+        awards.append(Award("🐺", "Lonewolf", f"Played {total_1v1s[player]:,d} 1v1s", player))
+
+    if total_crawls:
+        player = max(total_crawls, key=total_crawls.get)  # type: ignore
+        awards.append(
+            Award("🩸", "Knee Bleeder", f"Crawled {total_crawls[player]:,d} times", player)
+        )
+
+    if total_let_crawls:
+        player = max(total_let_crawls, key=total_let_crawls.get)  # type: ignore
+        awards.append(
+            Award("😈", "Ruthless", f"Let others crawl {total_let_crawls[player]:,d} times", player)
+        )
+
+    return awards
+
+
+def get_wrapped_season(ctx: Mapping[str, Any] | None = None) -> SeasonId | None:
+    """Get last season if only week ago.
+
+    :param ctx: context of jinja template
+    :return: season id
+    """
+    now = datetime(year=2024, month=10, day=7)
+    now_season = get_season(now)
+    week_ago_season = get_season(now - timedelta(days=7))
+    if now_season != week_ago_season:
+        return week_ago_season
+
+
+def wrapped_guard(connection: ASGIConnection, route_handler: BaseRouteHandler):
+    """Check if in wrapped period."""
+    wrapped_season = get_wrapped_season()
+    if wrapped_season is None:
+        raise NotAuthorizedException("This is a seasonal thing :)")
+
+
+@get("/wrapped/{season:str}", guards=[wrapped_guard])
+async def get_wrapped_page(season: SeasonId, db: sqlite3.Connection) -> Template:
+    query = "SELECT * FROM game WHERE season = ? ORDER BY timestamp"
+    season_games = tuple(Game(*row) for row in db.execute(query, (season,)))
+
+    pre_season_games = defaultdict(
+        int, db.execute("SELECT player, count(*) FROM rating GROUP BY player")
+    )
+
+    awards = get_game_score_awards(season_games, pre_season_games)
+
+    # TODO: most increase in games
+
+    player_name = dict(db.execute("SELECT id, name FROM player"))
+
+    return Template(
+        "wrapped.html",
+        context={"player_name": player_name, "season": season, "awards": awards},
+    )
